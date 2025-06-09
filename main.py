abb1304273e88bfd3374bc0907faf3236c964905
KiB454 <kingstonbarnes45@gmail.com>
2025-06-09 20:06:07 +0000

    added hello from Kingston

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return 'Hello from Flask!'
 
+@app.route('/kingston')
+def kingston():
+    return 'Hello from Kingston'
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
