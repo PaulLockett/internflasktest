ee01f7483bdf3561dfbaad7405493c2046ee225c
Paul Lockett <Plockett.pl@gmail.com>
2025-06-09 20:11:19 +0000

    added a route for my name

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return 'Hello from Paul!'
+
+@app.route('/Paul')
+def Paul():
+    return 'Hello from Paul 2!'
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
